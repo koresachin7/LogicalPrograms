97c1dd621a3b4fea1c459cfd84c673e162a0cbf3
koresachin7 <koresachin7@gmail.com>
2021-09-15 23:49:48 +0530

    Given input for you and generating coupon

--- src/com/bridgelabz/LogicalPrograms.java
+++ src/com/bridgelabz/LogicalPrograms.java
@@ -5,23 +5,25 @@ import java.util.Scanner;
 public class LogicalPrograms {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter number you want to reverse  :");
-        int num = sc.nextInt();
-        System.out.println("The reverse of the given number is:");
-        reverseNumber(num);
-        sc.close();
-    }
-    /* Method use:
-     * prints the same number if the number is less than 10
-     * calculate the remainder of the number using the modulo
-     */
-    public static void reverseNumber(int number) {
-        if (number < 10) {
-            System.out.println(number);
-            return;
-        } else {
-            System.out.print(number % 10);
-            reverseNumber(number / 10);
+        System.out.println("Enter coupon number");
+        char n = sc.next().charAt(0);
+
+        boolean[] isCollected = new boolean[n];
+        int count = 0;
+        int distinct = 0;
+        /*
+         * This loop repeatedly chose a random number and
+         * check whether it is a new one
+         */
+        while (distinct < n) {
+            int value = (int) (Math.random() * n);
+            count++;
+            if (!isCollected[value]) {
+                distinct++;
+                isCollected[value] = true;
+            }
         }
+        /* print the total number of cards collected */
+        System.out.println("Coupon code is : " + count);
     }
 }
